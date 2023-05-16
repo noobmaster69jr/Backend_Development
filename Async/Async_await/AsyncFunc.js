e4d5a47858a6db0086323034d12ece6b7fe682fd
noobmaster69jr <srashwin22@gmail.com>
2023-05-16 13:43:05 +0530

    Added async-await

---
+++ Async/Async_await/AsyncFunc.js
@@ -0,0 +1,7 @@
+/**
+ * we can declare function to async
+ * it allows the use of await keyword
+ * it allows consumption of promise using await
+ * Async function always convert return value to a promise
+ * await exists only inside async function
+ */
