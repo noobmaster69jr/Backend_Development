22ff338522e97e018d0309ec520f1165b2d1584c
noobmaster69jr <srashwin22@gmail.com>
2023-05-24 17:12:02 +0530

    mongoose create

--- MongoDB/mongoose/index.js
+++ MongoDB/mongoose/index.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose')
 
-console.log(mongoose)
+mongoose.connect("mongodb://localhost:27017/db")    //usr - db name
+  .then((res) => console.log("connected to MongoDB"))
+  .catch(err=>console.log("error : ",err));
+
+  let cars = mongoose.model("Cars",
+                mongoose.Schema({
+                    brand: String,
+                    model: String
+                }))
+
+cars.create({"brand": "maruti", "model":"Swift"})
+            .then(data => console.log('Sucess'))
+            .catch(err => console.log("Error"))
