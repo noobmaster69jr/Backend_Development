0286afea11d1e6f5b4cbb0d071144e4ab58f8b7a
Purushothaman <66981286+noobmaster69jr@users.noreply.github.com>
2023-12-13 18:53:47 +0530

    Update closure.js

--- Generators | Iterators /closure.js
+++ Generators | Iterators /closure.js
@@ -11,7 +11,25 @@ function process(){
 var fun = process()
 // when process seems like removed from the stack after return innerFunction
 // here still fun->innerFunction still has access to i
-console.log("first time calling ", fun())
-console.log("second time calling ", fun())
-console.log("third time calling ", fun())
+console.log("first time calling ", fun())     //1
+console.log("second time calling ", fun())    //2
+console.log("third time calling ", fun())     //3
+
+
+
+function fun(){
+    let i = 0;
+    let j = [1, 2, 3];
+    let k = 3;
+    function innerProcess(){
+        i += 1;
+        console.log(j);  // in closure, persistent memory for i, j is created and values are updated further but not k
+    }
+    return innerProcess;
+}
+
+let x = fun()
+let obj = {func : x}
+console.log(obj)
+
 
